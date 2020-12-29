d6f1423960f5f552d9ffb58ef0d20993a2647c50
Nikolaos Tsoukas <nick.tsoukas101@gmail.com>
2020-12-29 09:33:44 -0600

    Done with simple image gallery

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './index.css';
+import freckles from  './freckles.jpeg';
 
 function App() {
+  // This is the state and we need it. Currently on line 7 the value of images is and empty array -- no images yet 
+  const [images, setImages ] = useState([]);
+  /* We must use the useEffect hook. This is where we will make an api request and then set the images to the state
+     and map through them
+  */
+  useEffect(() => {
+    const getRedheads = async () => {
+        return await fetch("https://esits.org/tumblr-json.php?url=https://beautiful-redheads.tumblr.com", {method: 'GET'})
+          .then(res => res.json())
+          .then(res => res.posts
+            .filter(post => post.type ==='photo')
+            .map(photo => {
+              return {
+                img: photo["photo-url-1280"],
+                imgThumb: photo["photo-url-1280"]
+              }
+            })
+          )
+          .catch(error => console.log('WTF!', error))
+      }
+      
+      getRedheads()
+        .then((redheads) => setImages(() => [...redheads]));
+
+},[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <div style={{backgroundImage: `url(${freckles})`}} className="h-96 bg-black bg-center w-screen bg-cover"></div>
+     <div className="mason_container">
+            {
+          images.map((image, index) => {
+                  return (
+                    <div>
+                        <img src={image.img} key={index} className="mason_image shadow-lg hover:shadow-2xl" />
+                    </div>
+                    )
+                })
+            }   
+        </div>
+   </div>
   );
 }
 
